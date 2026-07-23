41dc7248cdc9911a8a32b8b97fe37c832327be14
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-07-23 19:22:59 -0400

    feat: create responsive catering site layout

--- src/CateringLayout.tsx
+++ src/CateringLayout.tsx
@@ -3,27 +3,21 @@
 import {Navbar} from "./Components/Navbar.tsx";
 import {HeroImage} from "./Components/HeroImage.tsx";
 import {FeatureHighlights} from "./Components/Features/FeatureHighlights.tsx";
+import {CateringFooter} from "./Components/Footer/CateringFooter.tsx";
 
 export const CateringLayout = () => {
     return (
-        <div className="grid grid-rows-[1fr_auto] w-full h-full ">
+        <div className="flex min-h-screen w-full min-w-0 flex-col overflow-x-hidden">
+            <Navbar />
 
-                <Navbar/>
-
-            <main className="pt-20">
-                <HeroImage/>
-                <FeatureHighlights/>
-
-
-                {/*<HeroImage/>*/}
-                {/*<HeroImage/>*/}
+            <main className="w-full min-w-0 flex-1 pt-20">
+                <HeroImage />
+                <FeatureHighlights />
             </main>
 
-            <footer className="border border-green-800">
-                <CateringLayout/>
+            <footer className="w-full min-w-0">
+                <CateringFooter />
             </footer>
-
-
         </div>
     );
 };
