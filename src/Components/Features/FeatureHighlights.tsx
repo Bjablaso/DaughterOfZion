fb2e8a82dd99182a488bfe511ef1d9bea51e7bc1
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-07-23 11:09:01 -0400

    Add: component for Layout for HeroImage

---
+++ src/Components/Features/FeatureHighlights.tsx
@@ -0,0 +1,9 @@
+// @flow
+
+export const FeatureHighlights = () => {
+    return (
+        <div className="h-[10vh] w-full border-4 border-purple-500">
+
+        </div>
+    );
+};
---
+++ src/Components/HeroImage.tsx
@@ -0,0 +1,9 @@
+// @flow
+
+export const HeroImage = () => {
+    return (
+        <div className="h-[75vh] w-full border-4 border-blue-500">
+
+        </div>
+    );
+};
