aa875e0a1dce962e8f1546b79884e41fcfed18d6
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-07-23 19:23:13 -0400

    feat: add hero section layout

--- src/Components/HeroImage.tsx
+++ src/Components/HeroImage.tsx
@@ -2,7 +2,7 @@
 
 export const HeroImage = () => {
     return (
-        <div className="h-[75vh] w-full border-4 border-blue-500">
+        <div className="min-h-[85vh] w-full border-4 border-blue-500">
 
         </div>
     );
